b5b3453cf65f49bae0e280d33621c2d668e420fa
Iddo Berger <iddoberger@gmail.com>
2016-05-17 17:44:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A Metaclass Singleton
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'singletonmetaclasss',
+  packages = ['singletonmetaclasss'],
+  version = '0.1',
+  description = 'A metaclasss singleton',
+  author = 'Iddo Berger',
+  author_email = 'iddoberger@gmail.com',
+  url = 'https://github.com/iddoberger/timingdecorator',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1.3',
+  keywords = ['timing',], # arbitrary keywords
+  classifiers = [],
+)
